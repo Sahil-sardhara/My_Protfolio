90c4a5ce2f9188fabc8ea485ab29d7b0fde0109c
Sahil Sardhara <sardharasahil47@gmail.com>
2025-06-20 20:51:25 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+# My_Protfolio
+=======
+>>>>>>> ee6e44f (intial commit)
 # awesome_protfolio
 
 A new Flutter project.
@@ -14,3 +20,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+<<<<<<< HEAD
+=======
+>>>>>>> 3c462c7 (Protfolio)
+>>>>>>> ee6e44f (intial commit)
--- lib/screens/phone_screen.dart
+++ lib/screens/phone_screen.dart
@@ -12,88 +12,129 @@ class PhoneScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CurrentState>(
-      builder: (context, currentState, _) {
-        return Container(
-          padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
-          decoration: BoxDecoration(
-            gradient: colorpalate[currentState.knobselected].gradient,
-          ),
-          child: Wrap(
-            crossAxisAlignment: WrapCrossAlignment.center,
-            alignment: WrapAlignment.center,
-            children: [
-              ...List.generate(
-                apps.length,
-                (index) => Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        right: 10,
-                        left: 10,
-                        top: 40,
-                        bottom: 20,
-                      ),
-                      child: Column(
-                        children: [
-                          CustomButton(
-                            margin: const EdgeInsets.only(bottom: 5),
-                            onPressed: () {
-                              if (apps[index].link != null) {
-                                currentState.launcher(apps[index].link!);
-                              } else if (apps[index].screen != null) {
-                                currentState.ChnagePhoneScreen(
-                                  apps[index].screen!,
-                                  false,
-                                  titlel: apps[index].title,
-                                );
-                              }
-                            },
-                            width: 45,
-                            height: 45,
-                            backgroundColor: apps[index].color,
-                            borderRadius:
-                                currentState.currentdevice ==
-                                        Devices.ios.iPhone13
-                                    ? 8
-                                    : 100,
-                            child:
-                                apps[index].icon != null
-                                    ? FaIcon(
-                                      apps[index].icon,
-                                      size: 20,
-                                      color: Colors.black,
-                                    )
-                                    : Icon(
-                                      Icons.device_unknown,
-                                      size: 20,
-                                      color: Colors.black,
-                                    ),
-                          ),
-                          SizedBox(
-                            width: 60,
-                            child: Center(
-                              child: Text(
-                                apps[index].title,
-                                style: const TextStyle(
-                                  fontSize: 11,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                ),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+        color: Colors.black.withOpacity(0.6),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Built with love",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(width: 6),
+            const Icon(Icons.favorite, color: Colors.red, size: 16),
+            const SizedBox(width: 6),
+            const Text(
+              "Using Flutter",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(width: 3),
+            IconButton(
+              onPressed: () {},
+              icon: const FaIcon(FontAwesomeIcons.flutter),
+            ),
+          ],
+        ),
+      ),
+      body: Consumer<CurrentState>(
+        builder: (context, currentState, _) {
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              // Get screen width
+              final screenWidth = constraints.maxWidth;
+              // Decide how many items per row based on screen size
+              int crossAxisCount;
+              if (screenWidth >= 1200) {
+                crossAxisCount = 5;
+              } else if (screenWidth >= 800) {
+                crossAxisCount = 4;
+              } else if (screenWidth >= 600) {
+                crossAxisCount = 3;
+              } else {
+                crossAxisCount = 2;
+              }
+
+              return Container(
+                padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+                decoration: BoxDecoration(
+                  gradient: colorpalate[currentState.knobselected].gradient,
+                ),
+                child: Center(
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    spacing: 20,
+                    runSpacing: 30,
+                    children: List.generate(apps.length, (index) {
+                      return SizedBox(
+                        width: screenWidth / crossAxisCount - 30,
+                        child: Column(
+                          children: [
+                            CustomButton(
+                              margin: const EdgeInsets.only(bottom: 8),
+                              onPressed: () {
+                                if (apps[index].link != null) {
+                                  currentState.launcher(apps[index].link!);
+                                } else if (apps[index].screen != null) {
+                                  currentState.ChnagePhoneScreen(
+                                    apps[index].screen!,
+                                    false,
+                                    titlel: apps[index].title,
+                                  );
+                                }
+                              },
+                              width: 55,
+                              height: 55,
+                              backgroundColor: apps[index].color,
+                              borderRadius:
+                                  currentState.currentdevice ==
+                                          Devices.ios.iPhone13
+                                      ? 8
+                                      : 100,
+                              child:
+                                  apps[index].icon != null
+                                      ? FaIcon(
+                                        apps[index].icon,
+                                        size: 20,
+                                        color: Colors.black,
+                                      )
+                                      : const Icon(
+                                        Icons.device_unknown,
+                                        size: 20,
+                                        color: Colors.black,
+                                      ),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              apps[index].title,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                          ],
+                        ),
+                      );
+                    }),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
